df6e3bc5f602853e13ee43d79a0ccf660f27d345
Georg Leciejewski <gl@salesking.eu>
2010-04-16 06:50:33 +0200

    readded jsonschema only for tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'spec'
 require "#{File.dirname(__FILE__)}/../lib/sk_api"
+require File.dirname(__FILE__) + '/../vendor/jsonschema-1.0.0/lib/jsonschema'
 
 
 def delete_test_data(doc, client)
